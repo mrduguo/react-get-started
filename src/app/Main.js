77ef9529659e40cab370ceac4f6a00ee06ceb61d
Guo Du <mrduguo@gmail.com>
2016-09-11 22:37:50 +0100

    init check in

---
+++ src/app/Main.js
@@ -0,0 +1,136 @@
+/**
+ * In this file, we create a React component
+ * which incorporates components provided by Material-UI.
+ */
+import React, {Component} from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import {deepOrange500} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import AppBar from 'material-ui/AppBar';
+
+const AppBarExampleIcon = () => (
+  <AppBar
+    title="Cloud Starter"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+    style={{position: 'fixed', top: 0}}
+  />
+);
+const CardExampleExpandable = () => (
+  <Card>
+    <CardHeader
+      title="Standalone"
+    />
+    <CardActions>
+      <FlatButton label="Start" />
+    </CardActions>
+    <CardText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </CardText>
+  </Card>
+);
+const CardExampleExpandable2 = () => (
+  <Card>
+    <CardHeader
+      title="Cluster"
+    />
+    <CardText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </CardText>
+    <CardActions>
+      <FlatButton label="Configure" />
+    </CardActions>
+  </Card>
+);
+
+const style = {
+  height: 400,
+  width: 300,
+  margin: 20,
+  display: 'inline-block',
+};
+
+const PaperExampleSimple = () => (
+  <div>
+    <Paper style={style} zDepth={3}>
+      <CardExampleExpandable/>
+    </Paper>
+    <Paper style={style} zDepth={3}>
+      <CardExampleExpandable2/>
+    </Paper>
+  </div>
+);
+
+
+const styles = {
+  container: {
+    textAlign: 'left',
+  },
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
+class Main extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  }
+
+  handleTouchTap() {
+    this.setState({
+      open: true,
+    });
+  }
+
+  render() {
+    const standardActions = (
+      <FlatButton
+        label="Ok"
+        primary={true}
+        onTouchTap={this.handleRequestClose}
+      />
+    );
+
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div style={styles.container}>
+          <AppBarExampleIcon/>
+          <h1>Cloud Starter</h1>
+          <PaperExampleSimple/>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Main;
