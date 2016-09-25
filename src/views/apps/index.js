2beeaf8dee52fc16dee428a72c7a3692442fbf59
Guo Du <mrduguo@gmail.com>
2016-09-26 00:10:49 +0100

    updates

--- src/views/apps/index.js
+++ src/views/apps/index.js
@@ -1,85 +1,150 @@
-import React, {Component} from "react";
-import CSSModules from "react-css-modules";
-import styles from "./Apps.scss";
-import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
-  from 'material-ui/Table';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd from 'material-ui/svg-icons/content/add';
-import ContentDelete from 'material-ui/svg-icons/action/delete';
-import ContentDeploy from 'material-ui/svg-icons/av/play-arrow';
-import ContentUndeploy from 'material-ui/svg-icons/av/stop';
-
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import IconButton from 'material-ui/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import React, {Component} from "react"
+import CSSModules from "react-css-modules"
+import styles from "./Apps.scss"
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from "material-ui/Table"
+import FloatingActionButton from "material-ui/FloatingActionButton"
+import ContentAdd from "material-ui/svg-icons/content/add"
+import ContentDelete from "material-ui/svg-icons/action/delete"
+import ContentDeploy from "material-ui/svg-icons/av/play-arrow"
+import ContentUndeploy from "material-ui/svg-icons/av/stop"
+import IconMenu from "material-ui/IconMenu"
+import MenuItem from "material-ui/MenuItem"
+import IconButton from "material-ui/IconButton"
+import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert"
 
 const tableData = [
   {
-    name: 'John Smith',
-    status: 'Employed',
+    name: 'Zookeeper',
+    status: 'Started',
   },
   {
-    name: 'Randal White',
-    status: 'Unemployed',
+    name: 'Kafka',
+    status: 'Started',
   },
   {
-    name: 'Stephanie Sanders',
-    status: 'Employed',
+    name: 'Schema Registry',
+    status: 'Started',
   },
   {
-    name: 'Steve Brown',
-    status: 'Employed',
+    name: 'Kafka Rest',
+    status: 'Started',
   },
   {
-    name: 'Joyce Whitten',
-    status: 'Employed',
+    name: 'Event Generator',
+    status: 'Started',
   },
   {
-    name: 'Samuel Roberts',
-    status: 'Employed',
+    name: 'Timezone Converter',
+    status: 'Stopped',
   },
   {
-    name: 'Adam Moore',
-    status: 'Employed',
+    name: 'Dashboard',
+    status: 'Stopped',
   },
 ];
 
 class Dashboard extends React.Component {
-
   state = {
-      stripedRows: false,
-      showRowHover: false,
-      selectable: true,
-      multiSelectable: false,
-      enableSelectAll: false,
-      deselectOnClickaway: true,
-      showCheckboxes: true,
-      height: '300px',
-    };
+    disableDeploy: true,
+    disableUndeploy: true,
+    disableDelete: true,
+  };
 
-  handleToggle = (event, toggled) => {
+  onDelete=(index)=>{
+    if(index=='selected'){
+      for(var i=tableData.length;i>0;i--){
+        if(tableData[i-1]['selected']){
+          tableData.splice(i-1, 1);
+        }
+      }
+    }else{
+      tableData.splice(index, 1);
+    }
     this.setState({
-      [event.target.name]: toggled,
-    });
+      disableDelete:true,
+      disableDeploy:true,
+      disableUndeploy:true,
+    })
+  }
+
+  onDeploy=(index)=>{
+    if(index=='selected'){
+      for(var row of tableData){
+        if(row['selected']){
+          row.status='Started';
+        }
+      }
+    }else{
+      tableData[index].status='Started';
+    }
+    this.setState({
+      disableDeploy:true,
+      disableUndeploy:false,
+    })
+  }
+
+  onUndeploy=(index)=>{
+    if(index=='selected'){
+      for(var row of tableData){
+        if(row['selected']){
+          row.status='Stopped';
+        }
+      }
+    }else{
+      tableData[index].status='Stopped';
+    }
+    this.setState({
+      disableDeploy:false,
+      disableUndeploy:true,
+    })
+  }
+
+  onRowSelection = (selectedRows) => {
+    switch (selectedRows) {
+      case 'none':
+        for (var row of tableData) {
+          row['selected']=false;
+        }
+        setTimeout(()=>{
+          this.setState({
+            disableDeploy: true,
+            disableUndeploy: true,
+            disableDelete: true,
+          })
+        },0)
+        return;
+      case 'all':
+        selectedRows=Array.from(Array(tableData.length).keys())
+      default:
+        var disableDeploy=true;
+        var disableUndeploy=true;
+        var disableDelete=true;
+        for (let [index, row] of tableData.entries()) {
+          if(selectedRows.includes(index)){
+            row['selected']= true;
+            if(row.status=='Started'){
+              disableUndeploy=false;
+            }else if(row.status=='Stopped'){
+              disableDeploy=false;
+            }
+            disableDelete=false;
+          }else{
+            row['selected']= false;
+          }
+        }
+        this.setState({
+          disableDeploy: disableDeploy,
+          disableUndeploy: disableUndeploy,
+          disableDelete: disableDelete,
+        })
+    }
+    return selectedRows;
   };
 
-  handleChange = (event) => {
-    this.setState({height: event.target.value});
-  };
-
-  onClick = (a,b,event) => {
-    console.log('a',a)
-    console.log('b',b)
-
-    event.stopPropagation();
-    event.preventDefault();
-  };
-
-  render() {
+  render () {
     return (
       <div>
-        <Table multiSelectable={true} onCellClick={this.onClick}>
+        <Table multiSelectable={true} onRowSelection={this.onRowSelection}>
           <TableHeader>
             <TableRow>
               <TableHeaderColumn tooltip="The ID">ID</TableHeaderColumn>
@@ -89,15 +154,13 @@ class Dashboard extends React.Component {
             </TableRow>
           </TableHeader>
           <TableBody
-            displayRowCheckbox={this.state.showCheckboxes}
+            displayRowCheckbox={true}
             deselectOnClickaway={false}
-            showRowHover={this.state.showRowHover}
-            stripedRows={this.state.stripedRows}
           >
-            {tableData.map( (row, index) => (
-              <TableRow key={index} selected={row.selected}>
+            {tableData.map((row, index) => (
+              <TableRow key={index} selected={row.selected?true:false}>
                 <TableRowColumn>
-                    {index}
+                  {index+1}
                 </TableRowColumn>
                 <TableRowColumn>{row.name}</TableRowColumn>
                 <TableRowColumn>{row.status}</TableRowColumn>
@@ -109,9 +172,9 @@ class Dashboard extends React.Component {
                       targetOrigin={{horizontal: 'right', vertical: 'top'}}
                       useLayerForClickAway={true}
                     >
-                      <MenuItem primaryText="Deploy" leftIcon={<ContentDeploy />} />
-                      <MenuItem primaryText="Undeploy" leftIcon={<ContentUndeploy />} />
-                      <MenuItem primaryText="Delete" leftIcon={<ContentDelete />} />
+                      <MenuItem primaryText="Deploy" disabled={row.status=='Started'?true:false} leftIcon={<ContentDeploy />} onTouchTap={()=>{this.onDeploy(index)}}/>
+                      <MenuItem primaryText="Undeploy" disabled={row.status=='Stopped'?true:false} leftIcon={<ContentUndeploy />} onTouchTap={()=>{this.onUndeploy(index)}}/>
+                      <MenuItem primaryText="Delete" leftIcon={<ContentDelete />} onTouchTap={()=>{this.onDelete(index)}}/>
                     </IconMenu>
                   </div>
                 </TableRowColumn>
@@ -119,16 +182,16 @@ class Dashboard extends React.Component {
             ))}
           </TableBody>
         </Table>
-        <FloatingActionButton styleName="buttonAdd" secondary={true}>
+        <FloatingActionButton styleName="buttonAdd" secondary={true} onTouchTap={()=>{alert('TODO')}}>
           <ContentAdd />
         </FloatingActionButton>
-        <FloatingActionButton styleName="buttonDelete" mini={true}>
+        <FloatingActionButton styleName="buttonDelete" mini={true} disabled={this.state.disableDelete} onTouchTap={()=>{this.onDelete('selected')}}>
           <ContentDelete />
         </FloatingActionButton>
-        <FloatingActionButton styleName="buttonDeploy" mini={true}>
+        <FloatingActionButton styleName="buttonUndeploy" mini={true} disabled={this.state.disableUndeploy} onTouchTap={()=>{this.onUndeploy('selected')}}>
           <ContentUndeploy />
         </FloatingActionButton>
-        <FloatingActionButton styleName="buttonUndeploy" mini={true}>
+        <FloatingActionButton styleName="buttonDeploy" mini={true} disabled={this.state.disableDeploy} onTouchTap={()=>{this.onDeploy('selected')}}>
           <ContentDeploy />
         </FloatingActionButton>
       </div>
